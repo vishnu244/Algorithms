7c5690db882c8d9e95cc3b286f9ae40c01d4ee0e
vishnu244 <vishnunali244@gmail.com>
2022-05-13 16:47:01 +0530

    [Vishnu]-Binary Search Logic

--- AlgorithmsPracticeProblems/Program.cs
+++ AlgorithmsPracticeProblems/Program.cs
@@ -3,16 +3,48 @@ using AlgorithmsPracticeProblems;
 
 Console.WriteLine("Welcome to Algorithms practice problems!");
 
-AnagramPractice anagram = new();
-Console.WriteLine("Please enter the two strings to find if they are anagrams or not. :");
-string firstword = Console.ReadLine();
-string secondword = Console.ReadLine();
 
-if (anagram.AnagramDetector(firstword, secondword))
+
+while (true)
 {
-    Console.WriteLine("Given words are Anagrams!");
+    Console.WriteLine("Please choose the option :\n1)Anagram\n2)Binary Search");
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            AnagramPractice anagram = new();
+            Console.WriteLine("Please enter the two strings to find if they are anagrams or not. :");
+            string firstword = Console.ReadLine();
+            string secondword = Console.ReadLine();
+
+            if (anagram.AnagramDetector(firstword, secondword))
+            {
+                Console.WriteLine("Given words are Anagrams!");
+            }
+            else
+            {
+                Console.WriteLine("No! Given words are not Anagrams!");
+            }
+            break;
+        case 2:
+            
+            BinarySearchWord bs = new ();
+            string[] arr = { "apple", "ball", "cat", "dog", "egg", "fish" };
+            foreach (string s in arr)
+            {
+                Console.WriteLine(s);
+            }
+            Array.Sort(arr);
+            Console.WriteLine("Please enter a string to be searched :");
+            string key = Console.ReadLine();
+            int result = bs.binary_search_iterative(arr, key);
+            if (result == -1)
+                Console.WriteLine("Element not present");
+            else
+                Console.WriteLine("Element found at "
+                                + "index " + result);
+            Console.WriteLine(result);
+            break;
+    }
 }
-else
-{
-    Console.WriteLine("No! Given words are not Anagrams!");
-}
+
