16ef94642bdf02cad343781fa32461642f74ec2a
vishnu244 <vishnunali244@gmail.com>
2022-05-13 16:31:34 +0530

    [Vishnu]-Anagram Logic

--- AlgorithmsPracticeProblems/Program.cs
+++ AlgorithmsPracticeProblems/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using AlgorithmsPracticeProblems;
+
 Console.WriteLine("Welcome to Algorithms practice problems!");
+
+AnagramPractice anagram = new();
+Console.WriteLine("Please enter the two strings to find if they are anagrams or not. :");
+string firstword = Console.ReadLine();
+string secondword = Console.ReadLine();
+
+if (anagram.AnagramDetector(firstword, secondword))
+{
+    Console.WriteLine("Given words are Anagrams!");
+}
+else
+{
+    Console.WriteLine("No! Given words are not Anagrams!");
+}
